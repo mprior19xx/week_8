b42a7893bc05420959d7cd93a3382d506c8427b9
Prior <m_prior85114@tss-hr420-dm25.fc.ca>
2019-03-04 16:08:52 -0500

    good

--- js/main.js
+++ js/main.js
@@ -12,7 +12,28 @@
 		// rewind and make it play
 		audio.currentTime = 0;
 		audio.play();
+
+		//grab div to be animated
+		let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+		key.classList.add('playing');
+
 	}
+
+	function removePlayingClass(event,){
+		// debugger;
+		if (event.propertyName !== "transform"){
+			return;
+		} 
+		else {
+			//remove playing class here from the active div
+			event.target.classList.remove('playing');
+			console.log("Transition is done");
+		}
+	}
+
+	const keys = Array.from(document.querySelectorAll('.keys'));
+
+	keys.forEach(key => key.addEventListener("transitionend", removePlayingClass));
 
 	window.addEventListener("keydown", playDrumKitSound);
 
